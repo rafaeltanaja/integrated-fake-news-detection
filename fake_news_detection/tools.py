ca1fd42ca1e678f8b97df73b2e2170807fb0dad2
Rafael Nicholas Tanaja <67319701+rafaeltanaja@users.noreply.github.com>
2025-01-11 14:22:39 +0700

    Update tools.py

--- fake_news_detection/tools.py
+++ fake_news_detection/tools.py
@@ -9,32 +9,6 @@ import torch.nn as nn
 from transformers import AutoModel, BertTokenizerFast
 from assets import processing
 
-class BERT_Arch(torch.nn.Module):
-    def __init__(self, bert_model_name):
-        super(BERT_Arch, self).__init__()
-        self.bert = AutoModel.from_pretrained(bert_model_name)
-        self.dropout = nn.Dropout(0.3)
-        self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(768, 128)
-        self.bn1 = nn.BatchNorm1d(128)
-        self.fc2 = nn.Linear(128, 2)
-
-    def forward(self, **inputs):
-        # Access input IDs and attention mask from the inputs dictionary
-        sent_id = inputs.get('input_ids')
-        mask = inputs.get('attention_mask')
-
-        # Pass input IDs and attention mask to the BERT model
-        _, cls_hs = self.bert(sent_id, attention_mask=mask, return_dict=False)
-
-        x = self.fc1(cls_hs)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.dropout(x)
-        x = self.fc2(x)
-        x = torch.log_softmax(x, dim=1)
-        return x
-
 with open('model/random_forest_model.pkl', 'rb') as file:
     loaded_data = pickle.load(file)
 
